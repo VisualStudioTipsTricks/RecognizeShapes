660722bb5e83af246b505377ffe7ae5c2e051f56
Damiani Liborio Igor <igor.damiani@hotmail.com>
2017-04-15 11:31:48 +0200

    -

--- RecognizeShapes/MainPage.xaml.cs
+++ RecognizeShapes/MainPage.xaml.cs
@@ -6,12 +6,9 @@ namespace RecognizeShapes
 {
     public sealed partial class MainPage : Page
     {
-        private InkAnalyzer inkAnalyzer = new InkAnalyzer();
-
         public MainPage()
         {
             this.InitializeComponent();
-            WritingArea.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Pen | CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Touch;
         }
 
         private void ClearButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
